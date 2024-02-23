4522bbc985f6692f42d0fa13c634671264a155c0
MohamedaliBz <Mohamedalibaazaoui521@gmail.com>
2024-02-23 21:53:39 +0100

    my_commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="card">
+    <img src="https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-980x653.jpg" alt="this image"></img>
+    <h3>Digital Image Size and Resolution</h3>
+    <p>Have you noticed that people get confused when talking about the size and resolution of an image?</p>
+    <p> Pixel dimensions are really the only thing you need to be concerned about.</p>
+    <form action="">
+      <input type="submit" value="Cancel" class="btn" id="btn1"/>
+      <input type="submit" value="Confirm" class="btn" id="btn2"/>
+    </form>
+  </div>
   );
 }
 
